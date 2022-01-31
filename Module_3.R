11f40339ded7064740a0a87428174c5e773d6280
Trevor214 <89483633+Trevor214@users.noreply.github.com>
2022-01-31 00:26:45 -0500

    Module 3 Assignment

---
+++ Module_3.R
@@ -0,0 +1,6 @@
+Name <- c("Jeb", "Donold", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC_poll <- c(4, 62, 51, 21, 2 , 14, 15)
+CBS_poll <- c(12,75,43,19,1,21,19)
+
+Election_2016_poll <- data.frame(Name, ABC_poll, CBS_poll)
+Election_2016_poll
