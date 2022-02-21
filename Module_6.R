7390a25628afd845fd85d1be1368644c525a3229
Trevor214 <89483633+Trevor214@users.noreply.github.com>
2022-02-20 19:24:38 -0500

    Add files via upload

---
+++ Module_6.R
@@ -0,0 +1,17 @@
+#Module 6 assignment
+# Considering two matrices A and B
+# find the sum and difference between the two
+
+# Initialize the two matrices
+A <- matrix(c(2,0,1,3), ncol = 2)
+B <- matrix(c(5,2,4,-1), ncol = 2)
+A
+B
+# Add the two matrices
+A + B
+# Subtract the two matrices
+A - B
+
+# Use diag() to build a matrix of size 4 with 4,1,2,3 in the diagonal
+diag(c(4,1,2,3), nrow = 4, ncol = 4)
+
