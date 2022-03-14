6cadd2868afda62048846d010350d91261431a07
Trevor214 <89483633+Trevor214@users.noreply.github.com>
2022-03-14 02:50:36 -0400

    Update Module9.R

--- Module9.R
+++ Module9.R
@@ -8,9 +8,9 @@ df1 <- df1[,-1]
 df1 <- na.omit(df1)
 df1
 #Base R graph
-plot(df1$year_start , df1$total_snow, main = "Snowfall at Paradise, Mt. Rainier National Park 1920 - 1930", xlab = "Year", ylab = "Snowfall in inches")
+plot(df1$year_start , df1$total_snow, main = "Snowfall at Paradise, Mt. Rainier National Park", xlab = "Year", ylab = "Snowfall in inches")
 # lattice package graph
-latticeSnow <- xyplot(total_snow~year_start, data = df1, main = "Snowfall at Paradise, Mt. Rainier National Park 1920 - 1930" , xlab = "Year", ylab = "Snowfall in inches")
+latticeSnow <- xyplot(total_snow~year_start, data = df1, main = "Snowfall at Paradise, Mt. Rainier National Park" , xlab = "Year", ylab = "Snowfall in inches")
 latticeSnow
 #ggplot graph
 ggplotSnow <- ggplot(df1, aes(x = year_start, y = total_snow, color = total_snow)) 
@@ -18,7 +18,7 @@ ggplotSnow <- ggplot(df1, aes(x = year_start, y = total_snow, color = total_snow
 ggplotSnow + 
   geom_point(shape = 8, size = 1, alpha = 1.6) + 
   geom_smooth() + 
-  labs(x = "Year", y = "Snowfall in inches") + ggtitle("Snowfall at Paradise, Mt. Rainier National Park 1920 - 1930") +
+  labs(x = "Year", y = "Snowfall in inches") + ggtitle("Snowfall at Paradise, Mt. Rainier National Park") +
   theme_wsj()
 
 
